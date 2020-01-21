89e71e764ec7efb9a263d0a7f1b7c63b710cbca8
Wilsonzhang25 <37731925+Wilsonzhang25@users.noreply.github.com>
2020-01-21 23:02:07 +0800

    Update fanfan.js

--- fanfan.js
+++ fanfan.js
@@ -120,7 +120,7 @@ let counter = 0;
 c2.width = c3.width = window.innerWidth;
 c2.height = c3.height = window.innerHeight;
 ctx1.fillStyle = '#000';
-const text = '雨漓';
+const text = '陈吟';
 let textWidth = 99999999;
 
 while (textWidth > window.innerWidth) {
